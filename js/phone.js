cabe4bdf0c888a33b48f5c3a97aa5291274c65d5
Anikcse18 <istiakjavedanik@gmail.com>
2024-06-05 15:04:55 +0600

    added show all function

--- js/phone.js
+++ js/phone.js
@@ -1,21 +1,35 @@
-const phone_API = async (searching) => {
+const phone_API = async (searching, isshowall) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searching}`
   );
   const data = await res.json();
   const phones = data.data;
-  displayphone(phones);
+  console.log(phones);
+  displayphone(phones, isshowall);
 };
 
-const displayphone = (phones) => {
+const displayphone = (phones, isshowall) => {
   const phone_container = document.getElementById("phone_container");
   phone_container.innerHTML = "";
+
+  let show_all = document.getElementById("show-all");
+  console.log("Status Prove: ", !isshowall);
+
+  if (phones.length > 10 && !isshowall) {
+    show_all.classList.remove("hidden");
+  } else {
+    show_all.classList.add("hidden");
+  }
+  if (!isshowall) {
+    phones = phones.slice(0, 10);
+  }
+
   phones.forEach((phone) => {
     const creatdiv = document.createElement("div");
     creatdiv.classList = "card bg-base-100 shadow-xl m-2 ";
     creatdiv.innerHTML = `
     <figure><img src="${phone.image}"
-    alt="Shoes" /> </figure>
+    alt="Shoes"/> </figure>
     <div class="card-body">
     <h2 class="card-title">${phone.phone_name}</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -25,14 +39,31 @@ const displayphone = (phones) => {
     </div>
     </div>`;
     phone_container.appendChild(creatdiv);
-    console.log(phone);
   });
+
+  loadder(false);
 };
 
 // search handel
 
-const searchhandeler = () => {
+const searchhandeler = (isshowall) => {
+  loadder(true);
   const search_field = document.getElementById("search_field");
-  phone_API(search_field.value);
+  phone_API(search_field.value, isshowall);
 };
 
+//Spinner here
+const loadder = (isloading) => {
+  const spinner_container = document.getElementById("spinner_container");
+
+  if (isloading) {
+    spinner_container.classList.remove("hidden");
+  } else {
+    spinner_container.classList.add("hidden");
+  }
+};
+//showall
+
+const handelshowall = () => {
+  searchhandeler(true);
+};
